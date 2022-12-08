a428d1f6ebc49a23d5bc8deb5d3a4b08e243f149
bastosydaniel <bastosydaniel@gmail.com>
2022-12-07 21:54:55 -0300

    final commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ROUTES } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { FuncionarioModule } from './funcionario';
+import { ListarFuncionarioComponent } from './listar-funcionario/listar-funcionario.component';
+import { EditarFuncionarioComponent } from './editar-funcionario/editar-funcionario.component';
+// import { CadastrarFuncionarioComponent } from './cadastrar-funcionario/cadastrar-funcionario.component';
+
+//Material imports
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // EditarFuncionarioComponent,
+    // CadastrarFuncionarioComponent,
+    // ListarFuncionarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    FuncionarioModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
